6182e4e312dbeab65f9afad1b2ec03d6cfb2034a
fiqriachmada <achmada1998@gmail.com>
2022-09-12 18:53:01 +0700

    new

---
+++ lib/constant/constant.dart
@@ -0,0 +1,11 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  // static const String mapBoxAccessToken =
+  //     'sk.eyJ1IjoiZmlxcmlhY2htYWRhIiwiYSI6ImNsN3luY2p3NDA3YWUzdmx3cG0yNTZ0YWoifQ.mwIRs8uMTy8lnnSEs5t9Vw';
+  static const String mapBoxAccessToken =
+      'pk.eyJ1IjoiZmlxcmlhY2htYWRhIiwiYSI6ImNqb2M3czhpZjFzdzQzcW9kZGFlaGI3d2gifQ.Plxy3SnDq7g7gH_KPXJkBw';
+  // static const String mapBoxStyleId = 'YOUR_STYLE_ID';
+  static const String mapBoxStyleId = 'cl7yotkxy000s14ryavxlzk8g';
+  static final myLocation = LatLng(-6.217025054500114, 106.80314277272761);
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:maps_on_flutter/constant/constant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 33, 32, 32),
+        title: const Text('Flutter MapBox'),
+      ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              minZoom: 5,
+              maxZoom: 18,
+              zoom: 13,
+              center: AppConstants.myLocation,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/fiqriachmada/{mapStyleId}/wmts?access_token={accessToken}',
+                // "https://api.mapbox.com/styles/v1/dhruv25/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
+                additionalOptions: {
+                  'mapStyleId': AppConstants.mapBoxStyleId,
+                  'accessToken': AppConstants.mapBoxAccessToken,
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
